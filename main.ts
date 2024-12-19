716b0b9b00c7e6a2000e6df4f59c6bf79fe68e41
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2024-12-19 08:34:35 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,34 +1,27 @@
 basic.showIcon(IconNames.SmallHeart)
 xiamiBoard.initXiaMiBoard()
-xiamiBoard.OLEDclear()
-xiamiBoard.setBrightness(90)
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_TAG_RECOGNITION)
-huskylens.writeOSD("DFRobot", 150, 30)
-huskylens.request()
 basic.forever(function () {
     huskylens.request()
-    if (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        if (huskylens.isAppear(7, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-            xiamiBoard.setIndexColor(0, 0xff0000)
-            xiamiBoard.setIndexColor(1, 0xff0000)
-        } else if (huskylens.isAppear(6, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-            xiamiBoard.setIndexColor(0, 0x00ff00)
-            xiamiBoard.setIndexColor(1, 0x00ff00)
-        } else if (huskylens.isAppear(5, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-            xiamiBoard.setIndexColor(0, 0x0000ff)
-            xiamiBoard.setIndexColor(1, 0x0000ff)
-        } else if (huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-            xiamiBoard.setIndexColor(0, 0x00ffff)
-            xiamiBoard.setIndexColor(1, 0x00ffff)
-        } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-            xiamiBoard.setIndexColor(0, 0xffff00)
-            xiamiBoard.setIndexColor(1, 0xffff00)
-        } else {
-            xiamiBoard.setIndexColor(0, 0x7f00ff)
-            xiamiBoard.setIndexColor(1, 0x7f00ff)
-        }
+    xiamiBoard.LED(0, 0, 1)
+    if (huskylens.isAppear(7, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        xiamiBoard.setIndexColor(0, 0xff0000)
+        xiamiBoard.setIndexColor(1, 0xff0000)
+    } else if (huskylens.isAppear(6, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        xiamiBoard.setIndexColor(0, 0x00ff00)
+        xiamiBoard.setIndexColor(1, 0x00ff00)
+    } else if (huskylens.isAppear(5, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        xiamiBoard.setIndexColor(0, 0x0000ff)
+        xiamiBoard.setIndexColor(1, 0x0000ff)
+    } else if (huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        xiamiBoard.setIndexColor(0, 0x00ffff)
+        xiamiBoard.setIndexColor(1, 0x00ffff)
+    } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        xiamiBoard.setIndexColor(0, 0xffff00)
+        xiamiBoard.setIndexColor(1, 0xffff00)
     } else {
-        basic.showIcon(IconNames.Butterfly)
+        xiamiBoard.setIndexColor(0, 0x7f00ff)
+        xiamiBoard.setIndexColor(1, 0x7f00ff)
     }
 })
